a1d17292f652a2c3ebdaac70d9868898539dc042
Talha_Altair <cooltalha27@gmail.com>
2022-08-28 19:33:14 +0530

    yes

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask, json, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def start():
 
-    # print("soemthing")
+    print("Tutorial")
 
     return jsonify({"talha":'something'})
 
 if __name__ == '__main__':
 
   app.run(host='0.0.0.0', port=9000, debug=True)
-
-  # app_1 = app.test_client()
-
-  # response =  app_1.get('/')
-
-  # print(response.get_data())
